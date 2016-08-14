941b4f5c15e73945ae8d5ee1ce83dccfbf5e147c
Arnout Engelen <arnouten@bzzt.net>
2016-08-14 20:15:57 +0200

    Correctly record language of EWD1202

--- meta/guess_languages.pl
+++ meta/guess_languages.pl
@@ -9,6 +9,8 @@ use Lingua::Identify qw(:language_identification);
 $/ = undef;
 
 my @files = <../sources/www.cs.utexas.edu/~EWD/transcriptions/*/*.html>;
+push(@files, '../sources/www.cs.utexas.edu/~EWD/transcriptions/EWD12xx/EWD 1202/EWD1202.html');
+
 foreach my $file (@files) {
   open FILE, $file or die "Couldn't open file $file: $!";
   my $content = <FILE>;
